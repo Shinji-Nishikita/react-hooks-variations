76a88412b4554897a87b1e50352ffe5163688776
Shinji-Nishikita <pizattu@gmail.com>
2021-12-29 00:14:21 +0900

    reference the DOM using useRef

--- src/App.js
+++ src/App.js
@@ -4,25 +4,36 @@ import './App.css';
 
 function App() {
 
-  // //例
+  // //pass the value
   // const number = useRef(100);
   // console.log("number is", number);
   // console.log("number.current is", number.current);//100
 
+  // // reference the DOM
   // const inputElement = useRef(null)
   // <input ref={inputElement} type="text" />
   // console.log(inputElement.current); // <input type="text" />
 
+  // // focus on input area
+  // const inputEl = useRef(null);
+  // const handleClick = () => {
+  //   inputEl.current.focus();
+  //   console.log("inputEl.current:", inputEl.current);//inputEl.current: <input type="text">
+  // };
+
+  // reference the DOM using useRef
   const inputEl = useRef(null);
+  const [text, setText] = useState("");
   const handleClick = () => {
-    inputEl.current.focus();
-    console.log("inputEl.current:", inputEl.current);//inputEl.current: <input type="text">
+    setText(inputEl.current.value);
   };
+  console.log("レンダリング！！");
 
   return (
     <div className="App">
       <input ref={inputEl} type="text" />
-      <button onClick={handleClick}>入力エリアをフォーカスする</button>
+      <button onClick={handleClick}>set text</button>
+      <p>テキスト : {text}</p>
     </div>
   );
 }
