4d808110eefd396a92a7bccf27ae6f2759dbffb3
Shinji-Nishikita <pizattu@gmail.com>
2021-12-28 23:56:56 +0900

    reference the DOM

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 
 function App() {
 
-  // pass the value
-  const number = useRef(100);
-  console.log("number is", number);
-  console.log("number.current is", number.current);//100
+  // //例
+  // const number = useRef(100);
+  // console.log("number is", number);
+  // console.log("number.current is", number.current);//100
+
+  // // reference the DOM
+  // const inputElement = useRef(null)
+  // <input ref={inputElement} type="text" />
+  // console.log(inputElement.current); // <input type="text" />
 
   return (
     <div className="App">
