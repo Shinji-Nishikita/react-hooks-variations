91496611bc1df95e328fe0fcf80632adcf92e2de
Shinji-Nishikita <pizattu@gmail.com>
2021-12-28 23:55:31 +0900

    pass the value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import React, { useState, useEffect, useRef } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // pass the value
+  const number = useRef(100);
+  console.log("number is", number);
+  console.log("number.current is", number.current);//100
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Test
     </div>
   );
 }
